edb2501a0763245b9f897f08d12f918b5385ec3a
Simeon Wuthier <simeonwuthier@gmail.com>
2022-03-01 12:27:34 -0700

    Read privilege to all

--- contract.sol
+++ contract.sol
@@ -15,27 +15,15 @@ contract Storage {
 
     // Retrieve a TSMI value given its corresponding index
     function retrieve(address user, uint256 index) public view returns (uint32){
-        if(hasReadPrivilege(msg.sender)) {
-            // Hash the user address with their index so that every user has their own storage space to store indexes: 0 to 2^256.
-            bytes32 hashIndex = keccak256(abi.encode(user, index));
-            return tmsimap[hashIndex];
-        } else{
-            return 0;
-        }
+        // Hash the user address with their index so that every user has their own storage space to store indexes: 0 to 2^256.
+        bytes32 hashIndex = keccak256(abi.encode(user, index));
+        return tmsimap[hashIndex];
     }
 
     // Only authorized users will return true, everyone else will return false
     function hasWritePrivilege(address user) public pure returns (bool) {
         if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Core network 1
         if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Core network 2
-        if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // etc...
-        return false;
-    }
-
-    // Only authorized users will return true, everyone else will return false
-    function hasReadPrivilege(address user) public pure returns (bool) {
-        if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Base station 1
-        if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Base station 2
         if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // etc...
         return false;
     }
