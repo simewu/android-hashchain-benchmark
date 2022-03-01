1e0408f0754d656516b57aa3354534d0c6fa3021
Simeon Wuthier <simeonwuthier@gmail.com>
2022-03-01 12:32:46 -0700

    Functioning contract

--- contract.sol
+++ contract.sol
@@ -14,7 +14,7 @@ contract Storage {
     }
 
     // Retrieve a TSMI value given its corresponding index
-    function retrieve(address user, uint256 index) public view returns (uint32){
+    function retrieve(uint256 index, address user) public view returns (uint32){
         // Hash the user address with their index so that every user has their own storage space to store indexes: 0 to 2^256.
         bytes32 hashIndex = keccak256(abi.encode(user, index));
         return tmsimap[hashIndex];
@@ -22,7 +22,7 @@ contract Storage {
 
     // Only authorized users will return true, everyone else will return false
     function hasWritePrivilege(address user) public pure returns (bool) {
-        if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Core network 1
+        if(user == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4) return true; // Core network 1
         if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // Core network 2
         if(user == 0x235d39CDA65c223611eA2A914FD686307FF389A4) return true; // etc...
         return false;
